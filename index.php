30ef46e6769cd462ef5350a0b9e32750daec00b7
“flaviusferrari” <flaviusferrari@gmail.com>
2021-08-07 15:05:40 -0300

    Melhoria no Design da página.

--- index.php
+++ index.php
@@ -3,13 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
     <title>Star Wars Characters</title>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 
 </head>
-<body>
+<body >
     <?php
         include("./class/swapi-class.php");
 
@@ -19,24 +20,32 @@
 
         $swCharacters = $sw->getDataApi();
     ?>
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-                <th>Personagem</th>
-                <th>Genero</th>
-                <th>Planeta Natal</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach($swCharacters->results as $character): ?>
-                
+    <div class="container">
+        <h1>Star Wars Characters</h1>
+        <table class="table table-striped table-bordered">
+            <thead class="thead-dark">
                 <tr>
-                    <td><?= $character->name; ?></td>
-                    <td><?= $character->gender; ?></td>
-                    <td><?= $sw->getDataResource($character->homeworld, 'planets', 'name'); ?></td>
+                    <th>Personagem</th>
+                    <th>Genero</th>
+                    <th>Planeta Natal</th>
                 </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php foreach($swCharacters->results as $character): ?>
+                    
+                    <tr>
+                        <td><?= $character->name; ?></td>
+                        <td><?= $character->gender; ?></td>
+                        <td><?= $sw->getDataResource($character->homeworld, 'planets', 'name'); ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+
+    <!-- SCRIPTS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+
 </body>
 </html>
