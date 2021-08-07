b4a81cb2c98b3fe0aada4e764c5c0220d22d2577
“flaviusferrari” <flaviusferrari@gmail.com>
2021-08-07 16:14:51 -0300

    Inserção da Modal

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
                         <td><?= $character->gender; ?></td>
                         <td>
                             <?= $sw->getDataResource($character->homeworld, 'planets', 'name'); ?>
-                            <a href="#" class="viewDetailsHomeworld">
+                            <a href="#" class="viewDetailsHomeworld" data-url="<?= $character->homeworld; ?>">
                                 <i class="fas fa-search"></i> Visualizar
                             </a>
                         </td>
@@ -47,8 +47,29 @@
         </table>
     </div>
 
+    <!-- MODAL -->
+    <div class="modal" tabindex="-1">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Modal body text goes here.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+            </div>
+        </div>
+    </div>
+
     <!-- SCRIPTS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
     <script src="./js/main.js"></script>
 </body>
